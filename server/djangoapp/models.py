8275e07bce7598b289536ba8d573f6992d541b0c
Jordi <jordipelaez05@gmail.com>
2023-07-25 11:04:41 -0400

    actualizacion modelos i administracion de clases

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,11 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    name = models.CharField(null = False, max_length =20,default = 'Model M')
+    manufacturer = models.CharField(null = False,max_length = 20, default = 'Tesla')
+    energy = models.CharField(null = False, max_length = 10,default = "Electric")
+    description = models.CharField (null = False,max_length = 50)
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,6 +23,13 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    name = models.ForeignKey(CarMake, null = False, on_delete = models.CASCADE)
+    dealer = models.IntegerField(null = False)
+    carType = models.CharField(null = False, max_length = 10, default = 'Sedan')
+    year = models.DateField(null = False)
+
+
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
